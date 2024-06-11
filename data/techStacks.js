35c449d3534249c522d9487df6a0a5ee2a83575b
Joshua <bolasajoshua73@gmail.com>
2024-06-11 17:07:28 +0800

    fixing a tech stack img data

--- data/techStacks.js
+++ data/techStacks.js
@@ -1,37 +1,44 @@
+import reactjs from "../assets/science.png";
+import tailwind from "../assets/Tailwind CSS.png";
+import html from "../assets/HTML5.png";
+import css from "../assets/css-3.png";
+import js from "../assets/js-file.png";
+import bootstrap from "../assets/Bootstrap.png";
+
 export const techStacks = [
   {
     id: 1,
-    img: "../../assets/HTML5.png",
+    img: html,
     techName: "HTML",
   },
 
   {
     id: 2,
-    img: "../../assets/css-3.png",
+    img: css,
     techName: "CSS",
   },
 
   {
     id: 3,
-    img: "../../assets/js-file.png",
+    img: js,
     techName: "JavaScript",
   },
 
   {
     id: 4,
-    img: "../../assets/science.png",
+    img: reactjs,
     techName: "ReactJS",
   },
 
   {
     id: 5,
-    img: "../../assets/Bootstrap.png",
+    img: bootstrap,
     techName: "Bootstrap",
   },
 
   {
     id: 6,
-    img: "../../assets/Tailwind CSS.png",
+    img: tailwind,
     techName: "Tailwind",
   },
 ];
